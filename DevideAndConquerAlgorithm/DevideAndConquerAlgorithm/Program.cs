08e25f37ce1bdfc0a16cdb3e8cb62145c06e0bd3
Sujith Kumar Yadav Pallapati <sujithkumar.pallapati@innRoad.local>
2017-02-16 17:02:57 +0530

    Devide and Conquer Alogirtham Test

---
+++ DevideAndConquerAlgorithm/DevideAndConquerAlgorithm/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevideAndConquerAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            try
+            {
+                stopwatch.Start();
+                int[] numbersList = GenerateMillionNumbers().ToArray();
+                stopwatch.Stop();
+                Console.WriteLine("Time taken for generating 10000000  numbers in milliseconds : " + stopwatch.Elapsed.TotalMilliseconds);
+
+                // Finding Peak in normal fashion
+                stopwatch.Reset();
+                stopwatch.Start();
+                int peak = FindPeakInSimpleManner(numbersList);
+                stopwatch.Stop();
+                Console.WriteLine("Peak Value is :" + peak);
+                Console.WriteLine("Time taken for finding peak in simple manner : " + stopwatch.Elapsed.TotalMilliseconds);
+
+                //Finding Peak using devide and conquer algo
+                stopwatch.Reset();
+                stopwatch.Start();
+                int divideAndConquerPeak = FindPeakUsingDivideAndConquer(numbersList);
+                stopwatch.Stop();
+                Console.WriteLine("Peak Value is :" + divideAndConquerPeak);
+                Console.WriteLine("Time taken for finding peak in Devide and Conquer manner : " + stopwatch.Elapsed.TotalMilliseconds);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception found " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static int FindPeakUsingDivideAndConquer(int[] numbersList)
+        {
+            int peak = 0;
+            int length = numbersList.Length;
+            int middleIndex = length/2;
+            int pointer = 0;
+            do
+            {  
+                pointer = (length- middleIndex) / 2;
+                if (numbersList[middleIndex] > numbersList[middleIndex - 1])
+                {
+                    peak = numbersList[middleIndex];
+                    middleIndex = middleIndex + pointer;                    
+                }
+                else {
+                    peak = numbersList[middleIndex];
+                    middleIndex = middleIndex - pointer;
+                }
+                
+            } while (pointer > 0 && pointer < numbersList.Length);
+
+            return peak;
+        }
+
+        private static int FindPeakInSimpleManner(int[] numbersList)
+        {
+            int peak = 0;
+            int length = numbersList.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (peak <= numbersList[i])
+                    peak = numbersList[i];
+            }
+            return peak;
+        }
+
+        private static List<int> GenerateMillionNumbers()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = -5000000; i <= 5000000; i++)
+            {
+                numbers.Add(i);
+            }
+            Console.WriteLine("Numbers Generated Successfully");
+            return numbers;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DevideAndConquerAlgorithm
-A test for how much Divide and Conquer Algorithm faster than normal search for Finding peak
+A test for how much Divide and Conquer Algorithm faster than normal search for Finding peak.
+
+In this test, Im creating a million random numbers ( in a a linear fasion) and trying to find peak. 
+1. Normal search where a <= b >=c way : i.e comparing with adjecent values.
+2. Devide and conquer algorithm
+
+Results are shown as given below:
+
+Numbers Generated Successfully
+Time taken for generating numbers in milliseconds : 173.8388
+Peak Value is :5000000
+Time taken for finding peak in simple manner : 33.5504
+Peak Value is :5000000
+Time taken for finding peak in Devide and Conquer manner : 0.2025
+
+
+Note : when you checked with less number of values ( i.e 10-100) you can results in reverse manner. i.e normal search is giving result faster. this is because, the logic which we wrote taking bit time.
+Numbers Generated Successfully
+Time taken for generating 10  numbers in milliseconds : 0.5855
+Peak Value is :5
+Time taken for finding peak in simple manner : 0.1113
+Peak Value is :5
+Time taken for finding peak in Devide and Conquer manner : 0.1358
